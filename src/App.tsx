57657e4490d5fd4f050ab7bead98ad43a94ebf22
LeConsulat2 <wooine2@gmail.com>
2025-06-18 10:36:25 +1200

    Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,46 @@ import Home from "./routes/home"
 import Profile from "./routes/profile"
 import Login from "./routes/login"
 import CreateAccount from "./routes/create-account"
+import { useEffect, useState } from "react"
+import LoadingScreen from "./components/loading-screen"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  }
+]);
 
 
 function App() {
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/profile",
-          element: <Profile />,
-        },
-      ],
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/create-account",
-      element: <CreateAccount />,
-    }
-  ]);
-
-
-
+  const [isLoading, setIsLoading] = useState(true);
+  const init = async() => {
+    setIsLoading(false)
+  }
+  useEffect(() => {
+    init();
+  }, []);
   return (
     <>
-      <RouterProvider router={router} />
-     
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
     </>
   )
 }
